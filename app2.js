b369423b70f79fd15f59e059d4d9205e6302d37e
Bilal Said Yılmaz <146930466+bloodneverlies@users.noreply.github.com>
2024-10-30 17:33:34 +0300

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,15 @@
+const{createApp}=Vue
+
+createApp({
+    data(){
+        return {
+      name:"Bilal",
+      job:"Mühendis",
+      website:"https://www.linkedin.com/in/bilal-said-y%C4%B1lmaz-a93a212a5/"
+        }
+    },
+    methods:{
+        
+    }
+   
+}).mount("#app");
