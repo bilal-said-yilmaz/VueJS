4b1be66eb2f34b8b4a721a503f59924ddb41bef4
Bilal Said Yılmaz <146930466+bloodneverlies@users.noreply.github.com>
2024-11-18 22:06:32 +0300

    Update app03.js

--- app3.js
+++ app3.js
@@ -3,8 +3,11 @@ const{createApp}=Vue
 createApp({
     data(){
         return {
-     age:20
-        }
+     age:20,
+     x:0,
+     y:0,
+     LinkedIN:"https://www.linkedin.com/in/bilal-said-y%C4%B1lmaz-a93a212a5/"
+    }
     },
     methods:{
         ekle:function(deger){
@@ -13,14 +16,19 @@ createApp({
         cikar:function(deger){
             return this.age--;
         },
-        ekle10:function(deger){
-            return this.age+=deger;
+        ekle10:function(deger2){
+            return this.age+=deger2;
         },
-        cikar10:function(deger){
-            return this.age-=deger;
-        },
+        cikar10:function(deger2){
+            return this.age-=deger2;
+        }, //methodlar arasına virgül konur
         updateXY(event){
-console.log(event);
+            console.log(event);
+            this.x=event.offsetX
+            this.y=event.offsetY
+        },
+        tikla(){
+            alert("TIKLAMA DEMİŞTİM ^_^")
         }
     }
    
