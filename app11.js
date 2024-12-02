ff2aed7489f4604ae052efa3fdacd3dc2b4b962c
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-02 23:16:11 +0300

    Update app11.js

--- app11.js
+++ app11.js
@@ -1,31 +1,36 @@
-const{createApp}=Vue
-
+const { createApp } = Vue;
 
 var one = createApp({
-    data(){
-        return{
-
+    data() {
+        return {
+            
         }
     },
 });
-one.component("element",{
-    template:"<p>Element Tag : {{name}}</p><button v-on:click=\"changeName()\">Change</button>",
-    data(){
-        return{
-            name:"Fiyatı gör"
+one.component("element", {
+    template: `
+        <h1>Element Tag : {{ name }}</h1>
+        <button v-on:click="changeName">Change</button>
+        <button v-on:click="rechangeName">ReChange</button>
+    `,
+    data() {
+        return {
+            name: "Fiyatı gör"
         }
     },
-    method:{
-        changeName(){
-            this.name="2.000.000"
-        }
+    methods: {
+        changeName() {
+            this.name = "2.000.000"
+        },
+        rechangeName(){
+            this.name ="Fiyatı gör"
+        },
     },
 });
 one.mount("#app-one");
-var two = createApp({
-    data(){
-        return{
 
-        }
+var two = createApp({
+    data() {
+        return {}
     },
 }).mount("#app-two");
