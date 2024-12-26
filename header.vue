ceea1d90159d4b82f7cffcdb6f0f85090de04d98
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-26 20:31:59 +0300

    Create header.vue

---
+++ header.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="eleman in liste">
+            {{ eleman }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+             liste : ["Homepage","About Us","Blog","Contact"]
+        }
+    },
+}
+</script>
