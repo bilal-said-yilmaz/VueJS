3cb2ea900efeb78a7c0ec5662270aa815603f6e7
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-26 20:33:49 +0300

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App2.vue'
+import Header from './header.vue'
+
+//const app = createApp(App)
+//app.component("app-header",Header)
+//app.mount('#app')
+
+createApp(App).mount('#app')
