8f0fffd696976f7d666aa2fb30c38b5a250e1222
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-26 19:47:23 +0300

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+    {{ title }}
+    <button v-on:click="changeTitle">Click here for change</button>
+    <button v-on:click="reverseTitle">Click here for reverse</button>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            title: 'Welcome!'
+        }
+    },
+    methods:{
+        changeTitle(){
+            this.title= 'Good Bye!'
+        },
+        reverseTitle(){
+            this.title= 'Welcome!'
+        }
+    }
+}
+</script>
