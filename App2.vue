fa7c800ae4fcfe422ea60581a10140d059e73b40
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-26 20:29:30 +0300

    Create App2.vue

---
+++ App2.vue
@@ -0,0 +1,17 @@
+<template>
+    <app-header></app-header>
+</template>
+
+<script>
+import Header from "./header.vue" // locale component with createApp(App).mount('#app')
+export default{
+    components:{
+        "app-header":Header
+    },
+    data(){
+        return{
+            
+        }
+    },
+}
+</script>
